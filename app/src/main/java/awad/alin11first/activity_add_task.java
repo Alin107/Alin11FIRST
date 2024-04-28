4f79191e92348bab11dae6ce91f914641d6fc24c
Admin <bahaa.234568@gmail.com>
2024-04-28 14:59:18 +0300

    gjgjgj

---
+++ app/src/main/java/awad/alin11first/activity_add_task.java
@@ -0,0 +1,14 @@
+package awad.alin11first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_add_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+    }
+}
